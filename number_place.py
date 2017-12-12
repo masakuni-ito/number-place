eb0b54a792c5ae52d92c16472981b239fbf6e007
masakuni <>
2017-12-12 21:33:41 +0900

    bug fix

--- number_place.py
+++ number_place.py
@@ -6,26 +6,31 @@ from pprint import pprint
 class NumberPlace:
 
     @property
-    def board(self):
-        return self._board
+    def debug(self):
+        return self._table
 
-    def __init__(self, max_number=9):
-        self._board = []
+    @property
+    def table(self):
+        return self._table
+
+    def __init__(self, max_number=9, debug=False):
+        self._debug = debug
+        self._table = []
         self._has_area_check = False
         self._area_length = 0
         self._depth = 0
 
         # 初期化
-        tmp_board = []
-        for row_num in range(1, max_number+1):
+        tmp_table = []
+        for row_num in range(0, max_number):
 
             row = []
-            for col_num in range(1, max_number+1):
+            for col_num in range(0, max_number):
                 row.append(0)
 
-            tmp_board.append(row)
+            tmp_table.append(row)
 
-        self._board = tmp_board
+        self._table = tmp_table
 
         # エリアでのチェック行えるか
         # 平方根が自然数であること
@@ -36,43 +41,41 @@ class NumberPlace:
             self._has_area_check = True
             self._area_length = int_sqrt_num
 
-    def new_board(self, max_number=9):
+    def new_table(self, max_number=9):
         # 初期化
-        tmp_board = []
-        for row_num in range(1, max_number+1):
+        tmp_table = []
+        for row_num in range(0, max_number):
 
             row = []
-            for col_num in range(1, max_number+1):
+            for col_num in range(0, max_number):
                 row.append(0)
 
-            tmp_board.append(row)
+            tmp_table.append(row)
 
-        return tmp_board
+        return tmp_table
 
-    def set_board(self, board):
-        self._board = []
+    def set_table(self, table):
+        self._table = []
         self._has_area_check = False
         self._area_length = 0
         self._depth = 0
 
         # 受け入れられるのはx*xの二次元配列のみ
-        if not isinstance(board, list) or len(board) <= 0:
-            raise Exception('invalid board: this board is not list.')
+        if not isinstance(table, list) or len(table) <= 0:
+            raise Exception('invalid table: this table is not list.')
 
-        max_number = len(board)
+        max_number = len(table)
 
         # 縦横同一の長さ
-        for row in board:
+        for row in table:
             if not isinstance(row, list) or len(row) != max_number:
-                raise Exception('invalid board: this row is not list or misshapen square.')
+                raise Exception('invalid table: this row is not list or misshapen square.')
 
         # 数値以外は受け入れない
         for row_num in range(0, max_number):
             for col_num in range(0, max_number):
-                if not isinstance(board[row_num][col_num], int):
-                    raise Exception('invalid board: invalid values.')
-
-        self._board = copy.deepcopy(board)
+                if not isinstance(table[row_num][col_num], int):
+                    raise Exception('invalid table: invalid values.')
 
         # エリアでのチェック行えるか
         # 平方根が自然数であること
@@ -83,139 +86,229 @@ class NumberPlace:
             self._has_area_check = True
             self._area_length = int_sqrt_num
 
-    def __get_max_number(self, board):
-        return len(board)
+        # 初期値確認
+        self.__validate_table(table)
 
-    def fill(self, board=None):
+        self._table = copy.deepcopy(table)
 
-        if board is None:
-            board = copy.deepcopy(self._board)
+    def __validate_table(self, table):
+        
+        max_number = self.__get_max_number(table)
 
-        ret = self.__recursive_fill(board)
-
-        if ret == False:
-            return False
-
-        self._board = ret
-
-        return True
-
-    def __recursive_fill(self, board):
-
-        print('|' * self._depth)
-
-        # 現状入力されている値から、入力できる値を入れ込む
-        board = self.__fix(board)
-
-        # 一つ一つ入力して齟齬が発生しないか確かめる
-        max_number = self.__get_max_number(board)
         for row_num in range(0, max_number):
             for col_num in range(0, max_number):
 
-                if board[row_num][col_num] != 0:
+                # 横マスの現状入力値
+                row = self.__get_row_numbers(table, row_num)
+
+                # 既に不整合が起きている
+                for i in range(1, max_number+1):
+                    if row.count(i) >= 2:
+                        raise Exception('invalid table: The value already entered is incorrect.')
+
+                # 縦マスの現状入力値
+                col = self.__get_col_numbers(table, col_num)
+
+                # 既に不整合が起きている
+                for i in range(1, max_number+1):
+                    if col.count(i) >= 2:
+                        raise Exception('invalid table: The value already entered is incorrect.')
+
+                # エリアでのチェックが必要かどうか
+                if self._has_area_check:
+                    area = self.__get_area_numbers(table, row_num, col_num)
+
+                    # 既に不整合が起きている
+                    for i in range(1, max_number+1):
+                        if area.count(i) >= 2:
+                            raise Exception('invalid table: The value already entered is incorrect.')
+
+        return True
+
+    def __get_max_number(self, table):
+        return len(table)
+
+    def __get_row_numbers(self, table, row_num):
+        return table[row_num]
+
+    def __get_col_numbers(self, table, col_num):
+        col = []
+
+        max_number = self.__get_max_number(table)
+        for i in range(0, max_number):
+            col.append(table[i][col_num])
+
+        return col
+
+    def __get_area_numbers(self, table, row_num, col_num):
+        area = []
+        
+        # 現在マスが属するエリアの最小値位置を求めて基点とする
+        base_point_row = math.floor(row_num / self._area_length) * self._area_length
+        base_point_col = math.floor(col_num / self._area_length) * self._area_length
+        
+        # 現在のエリアに存在する入力値を取得する
+        for area_row_num in range(base_point_row, base_point_row + self._area_length):
+            for area_col_num in range(base_point_col, base_point_col + self._area_length):
+                area.append(table[area_row_num][area_col_num])
+
+        return area
+
+    def fill(self, table=None):
+
+        if table is None:
+            table = copy.deepcopy(self._table)
+
+        filled_table = self.__recursive_fill(table)
+
+        if filled_table == False: return False
+
+        self._table = filled_table
+
+        return True
+
+    def __recursive_fill(self, table):
+
+        print('|' * (self._depth + 1))
+
+        # 現状入力されている値から、入力できる値を入れ込む
+        if self._debug: pprint(">>> fixed")
+        table = self.__fix(copy.deepcopy(table))
+        if self._debug: pprint(table)
+        if self._debug: pprint("<<< fixed")
+
+        # 一つ一つ入力して齟齬が発生しないか確かめる
+        max_number = self.__get_max_number(table)
+        for row_num in range(0, max_number):
+            for col_num in range(0, max_number):
+
+                # 既に入っているものに対しては走査しない
+                if table[row_num][col_num] != 0:
                     continue
 
-                suspicious_numbers = self.__get_suspicious_numbers(board, row_num, col_num)
+                if self._debug: pprint("row: {0}, col: {1}".format(row_num, col_num))
+                suspicious_numbers = self.__get_suspicious_numbers(table, row_num, col_num)
+
+                # このマスに当てはまるものがないため、前提のマスが誤っている
                 if suspicious_numbers == False:
+                    if self._debug: pprint("suspicious_numbers is False")
                     return False
 
+                right_number = False
                 for suspicious_number in suspicious_numbers:
 
                     # 仮に入れ込んで走査する
-                    tmp_board = copy.deepcopy(board)
-                    tmp_board[row_num][col_num] = suspicious_number
+                    tmp_table = copy.deepcopy(table)
+                    tmp_table[row_num][col_num] = suspicious_number
 
                     self._depth += 1
 
-                    ret = self.__recursive_fill(tmp_board)
+                    if self._debug: pprint(">>> try row: {0}, col: {1}, try: {2} of {3}".format(row_num, col_num, suspicious_number, suspicious_numbers))
+                    if self._debug: pprint(tmp_table)
+                    filled_table = self.__recursive_fill(tmp_table)
+                    if self._debug: pprint("<<< try row: {0}, col: {1}, try: {2} of {3}".format(row_num, col_num, suspicious_number, suspicious_numbers))
 
                     self._depth -= 1
 
-                    # 入れ込んだ結果が誤っている
-                    if ret == False:
+                    # このマスに suspicious_number を入れると再帰先で齟齬が起きる
+                    if filled_table == False:
                         continue
 
+                    if self._debug: pprint(">>> result")
+                    if self._debug: pprint(filled_table)
+                    if self._debug: pprint("<<< result")
+
                     # 入れ込んだ結果、現時点で誤りが出ていないので正当な値と仮に決定する
-                    board = tmp_board
+                    table = filled_table
+
+                    # 再帰先でも齟齬は起きなかった
+                    right_number = True
                     break
 
-        return board
+                # 走査してみた結果、再帰先のマスで齟齬が生まれるため、
+                # この suspicious_numbers は使えず、前提のマスが誤っている
+                if right_number == False:
+                    return False
 
-    def __fix(self, board):
+        print('|' * (self._depth + 1))
+        return table
 
-        max_number = self.__get_max_number(board)
+    def __fix(self, table):
+
+        max_number = self.__get_max_number(table)
 
         while True:
             changed = False
             for row_num in range(0, max_number):
                 for col_num in range(0, max_number):
 
-                    if board[row_num][col_num] != 0:
+            
+                    # 既に入っているものに対しては走査しない
+                    if table[row_num][col_num] != 0:
                         continue
 
-                    fixed_number = self.__get_fixed_number(board, row_num, col_num)
+                    # row_num, col_numにおいて、一つに確定できる数を求める
+                    fixed_number = self.__get_fixed_number(table, row_num, col_num)
 
                     if fixed_number == False:
                         continue
 
-                    board[row_num][col_num] = fixed_number
+                    table[row_num][col_num] = fixed_number
                     changed = True
 
+            # 一つでも入力値に変動があったらループ
             if changed == False:
                 break
 
-        return board
+        return table
 
-    def __get_fixed_number(self, board, row_num, col_num):
+    def __get_fixed_number(self, table, row_num, col_num):
 
-        suspicious_numbers = self.__get_suspicious_numbers(board, row_num, col_num)
+        suspicious_numbers = self.__get_suspicious_numbers(table, row_num, col_num)
         if suspicious_numbers == False or len(suspicious_numbers) != 1:
             return False
-        return suspicious_numbers.pop()
 
-    def __get_suspicious_numbers(self, board, row_num, col_num):
+        # 入力値が一つに確定できた
+        fix_num = suspicious_numbers.pop()
+        if self._debug: pprint("row: {0}, col: {1}, fix: {2}".format(row_num, col_num, fix_num))
 
-        max_number = self.__get_max_number(board)
+        return fix_num
 
-        # 横マスで入力されている数
-        row = board[row_num]
+    def __get_suspicious_numbers(self, table, row_num, col_num):
 
-        # 横マスで入力可能な数
+        max_number = self.__get_max_number(table)
+
+        # 横マスの現状入力値
+        row = self.__get_row_numbers(table, row_num)
+
+        # 横マスで入力可能な入力値
         allow_row_numbers = []
         for i in range(1, max_number+1):
             if i not in row:
                 allow_row_numbers.append(i)
 
-        # 縦マスで入力されている数
-        col = []
-        for i in range(0, max_number):
-            col.append(board[i][col_num])
+        # 縦マスの現状入力値
+        col = self.__get_col_numbers(table, col_num)
 
-        # 縦マスで入力可能な数
+        # 縦マスで入力可能な入力値
         allow_col_numbers = []
         for i in range(1, max_number+1):
             if i not in col:
                 allow_col_numbers.append(i)
  
-        # エリアで入力可能な数
+        # エリアでのチェックが必要かどうか
         allow_area_numbers = []
         if self._has_area_check:
 
-            # 同一エリア内で最も小さいrowとcol
-            # 基点とする
-            base_point_row = math.floor(row_num / self._area_length) * self._area_length
-            base_point_col = math.floor(col_num / self._area_length) * self._area_length
+            # エリアマスの現状入力値
+            area = self.__get_area_numbers(table, row_num, col_num)
 
-            # 現状のエリアの中にある値
-            area = []
-            for area_row_num in range(base_point_row, base_point_row + self._area_length):
-                for area_col_num in range(base_point_col, base_point_col + self._area_length):
-                    area.append(board[area_row_num][area_col_num])
-
+            # エリアマスで入力可能な入力値
             for i in range(1, max_number+1):
                 if i not in area:
                     allow_area_numbers.append(i)
         else:
+            # エリアマスでのチェックが不要であれば、エリアマスとしては全てを許容する
             for i in range(1, max_number+1):
                 allow_area_numbers.append(i)
 
@@ -249,7 +342,7 @@ class NumberPlace:
         if found == False:
             return False
 
-        # それぞれに共通して入力可能な値しか入力はできない
+        # 全てに共通して入力可能な値しか入力はできないため抽出する
         suspicious_numbers = []
         for i in range(1, max_number+1):
             if i in allow_row_numbers and i in allow_col_numbers and i in allow_area_numbers:
@@ -260,14 +353,21 @@ class NumberPlace:
 def main():
     number_place = NumberPlace()
 
-    # set board
-    board = number_place.new_board(4)
-    board[0][0] = 4
-    board[1][0] = 3
-    number_place.set_board(board)
+    # set table
+    table = number_place.new_table(9)
+    #table = [
+    #    [0, 0, 0, 0, 0, 0, 0],
+    #    [0, 0, 0, 0, 0, 0, 0],
+    #    [0, 0, 0, 0, 0, 0, 0],
+    #    [0, 0, 0, 0, 0, 0, 0],
+    #    [0, 0, 0, 0, 0, 0, 0],
+    #    [0, 0, 0, 0, 0, 0, 0],
+    #    [0, 0, 0, 0, 0, 0, 0]
+    #]
+    number_place.set_table(table)
 
     number_place.fill()
-    pprint(number_place.board)
+    pprint(number_place.table)
 
 if __name__ == '__main__':
     main()
